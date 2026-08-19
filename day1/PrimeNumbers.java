bbb6312cfccf90bae835b03ff85b743f3a82d737
Koustav Singh <onlyforhomies.k@proton.me>
2026-08-19 20:40:09 +0530

    fix: correct prime number logic

--- day1/PrimeNumbers.java
+++ day1/PrimeNumbers.java
@@ -7,8 +7,9 @@ class PrimeNumbers {
         for (int i = 2; i <= n / 2; i++) {
             if (n % i == 0)
                 return false;
+        }
+
         return true;
-      }
     }
 
     void displayPrimes() {
